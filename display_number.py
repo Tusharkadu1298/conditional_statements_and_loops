d60a33ff034b17af7afdfba1302284dbc329f801
Tushar Dattaguru Kadu <131634787+Tusharkadu1298@users.noreply.github.com>
2023-05-05 17:03:05 +0530

    Create display_number.py

---
+++ display_number.py
@@ -0,0 +1,12 @@
+# 5  Write a program to display numbers from -10 to -1.
+
+# using forloops
+for number in range (-10,0,1):
+    print(number)
+    
+    
+# using while loops
+number = -10
+while number < 0:
+    print(number)
+    number += 1
